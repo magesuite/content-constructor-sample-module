615652bc8286225018408483f616d778d3006e07
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2022-03-07 17:19:18 +0100

    Sample module BE

---
+++ Block/Adminhtml/Component/HelloWorld.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MageSuite\ContentConstructorSampleModule\Block\Adminhtml\Component;
+
+class HelloWorld extends \Magento\Framework\View\Element\Template
+{
+    protected $_template = 'MageSuite_ContentConstructorSampleModule::components/configurators/hello_world.phtml';
+}
---
+++ Block/Component/HelloWorld.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MageSuite\ContentConstructorSampleModule\Block\Component;
+
+class HelloWorld extends \MageSuite\ContentConstructorFrontend\Block\Component\AbstractComponent
+{
+    protected $_template = 'MageSuite_ContentConstructorFrontend::component/hello_world.phtml';
+}
