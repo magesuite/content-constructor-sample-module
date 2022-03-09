b117fc2131e226a7ef8dc35404525ad3a319f2c2
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2022-03-09 11:09:31 +0100

    Fixed paths. Added proper di.xml initialization

--- Block/Component/HelloWorld.php
+++ Block/Component/HelloWorld.php
@@ -4,5 +4,5 @@ namespace MageSuite\ContentConstructorSampleModule\Block\Component;
 
 class HelloWorld extends \MageSuite\ContentConstructorFrontend\Block\Component\AbstractComponent
 {
-    protected $_template = 'MageSuite_ContentConstructorFrontend::component/hello_world.phtml';
+    protected $_template = 'MageSuite_ContentConstructorSampleModule::component/hello_world.phtml';
 }
